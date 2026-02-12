0841d0df60f2b77c49b931ca6f1d2fada93a2ede
JP <jp@johnprinz.com>
2026-02-12 15:50:20 -0500

    Added NoData Prop to empty GET request;

--- models/User.js
+++ models/User.js
@@ -3,6 +3,9 @@ const db = require('../config/database');
 const User = {
     getAllUsers: async function() {
         const [rows] = await db.query('SELECT * FROM users');
+        if (rows.length <= 0){
+            rows[0] = {"NoData": "true"};
+        }
         return rows;
     },
     create: async function(name, email) {
