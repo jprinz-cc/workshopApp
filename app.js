7810bd6850b5dd30f412ab66ce37f2df54ef108b
JP <jp@johnprinz.com>
2026-04-02 15:36:04 -0400

    Changed origin to *;

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://two500workshopreact.onrender.com/"],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
